5a57e61a2bdfb73439cb4a4f5fbec4004976536e
ForsyteRun <forsyterun@gmail.com>
2022-11-16 09:18:02 +0200

    Sass & Layout

---
+++ components/Layout.tsx
@@ -0,0 +1,19 @@
+import { FC, ReactNode } from "react"
+import Footer from "./Footer"
+import Header from "./Header"
+
+type Props = {
+   children: ReactNode
+};
+
+const Layout: FC<Props> = ({children}) => {
+  return (
+   <>
+      <Header/>
+         {children}
+      <Footer/>
+   </>
+   )
+}
+
+export default Layout
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <Layout>
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </Layout>
+  );
+};
+
+export default App;
