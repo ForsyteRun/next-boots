0c7b45aa46f405dc91344fb9154883b9cbc5ec03
ForsyteRun <forsyterun@gmail.com>
2022-11-16 10:32:08 +0200

    dev-TS ThemeProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,61 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { Box, Container,CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/system";
+declare module '@mui/material/styles' {
+  interface Theme {
+    breakpoints: {
+      values: {
+        xs: number
+        sm: number
+        md: number
+        lg: number
+        xl: number    
+     }
+   }
+  }
+   interface ThemeOptions {
+    breakpoints?: {
+      values?: {
+        xs: number
+        sm: number
+        md: number
+        lg: number
+        xl: number    
+      }
+    }
+  }
+};
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1080,
+      xl: 1500
+    }, 
+  },
+});
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <Layout>
-      <main>
-        <Component {...pageProps} />
-      </main>
-    </Layout>
+    <>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+      <Container maxWidth='lg'>
+        <Box sx={{ bgcolor: "#ffff", height: "100vh" }}>
+          <Layout>
+            <main>
+              <Component {...pageProps} />
+            </main>
+          </Layout>
+        </Box>
+      </Container>         
+      </ThemeProvider>
+    </>
   );
 };
 
