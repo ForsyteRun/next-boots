0bfa76078be6dc93b62ca6ff4dd82d1564291ab6
ForsyteRun <forsyterun@gmail.com>
2022-11-17 17:26:14 +0200

    header done

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,14 +10,14 @@ import s from './../styles/Header.module.scss'
 const Header: FC = () => {
   return (
    <header>
-    <Stack direction={"row"}>
+    <Stack direction={"row"} className={s.conteiner}>
       <Image src={logo} alt='logo' />
       <Stack className={s.headerCenter}>
         <h3 className={s.title}>REACT SNEAKERS</h3>
         <div className={s.subTitle}>Магазин лучших кроссовок</div>
       </Stack>
       <Stack direction={"row"} justifyContent={'space-between'} width='196px'>
-        <Stack direction={"row"}>
+        <Stack width='95px' className={s.shop}>
           <ShoppingCartOutlinedIcon color="primary"/>
           <span>1205uah</span>          
         </Stack>
