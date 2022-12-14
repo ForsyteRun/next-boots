b2933293b3b1afafe6b504163df803e8954f23f1
ForsyteRun <forsyterun@gmail.com>
2022-12-14 21:17:32 +0200

    rexactor: add tacks

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,13 +14,16 @@ import s from "./../styles/App.module.scss";
 
 //todo: Ошибка в консоли по компоненте Main "не верный пропс по id"
 //todo: симбиоз с MuiTheme не осуществлен, брейкпоинты. Какой лучше шрифт: Mui or Next.js. Responsive font
+//todo: сделать адаптив
 //todo: 'жирный' useContext. Не лучше ли подключить Redux.
 //todo: React.Memo
 //todo: не много ли useState?
 //todo: ворнинг на experimental feature (fontLoaders) in next.config.js.
 //todo: номерация заказов на странице Orders
 //todo: нужно ли Promise.All???
-
+//todo: перевести на FireBase
+//todo: Layout в Index.tsx и сделать SSR
+ 
 const inter = Inter({ subsets: ["latin"] });
 declare module "@mui/material/styles" {
   interface Theme {
